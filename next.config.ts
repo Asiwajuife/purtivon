a9f8580fa984dcec0d487b51d5301faa02bbbd7b
Asiwajuife <101390789+Asiwajuife@users.noreply.github.com>
2026-04-01 18:46:37 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  poweredByHeader: false,
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.purtivon.com",
+      },
+    ],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client", "bcryptjs"],
+  },
 };
-
 export default nextConfig;
